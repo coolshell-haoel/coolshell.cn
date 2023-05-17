1b1317d1fb96657d473c636a9aa6bd26a9b541e7
CanftIn <wwc7033@gmail.com>
2023-05-17 15:07:35 +0800

    fix page lost

--- coolshell.cn/asyncseries.js
+++ coolshell.cn/asyncseries.js
@@ -1,2 +1,34 @@
-(function(){var Async;Async=(function(){function Async(){this.callbacks=[];this.running=false;}
-Async.prototype.series=function(func){if(this.running){this.callbacks.push(func);}else{this.running=true;func();}};Async.prototype.callback=function(){if(this.callbacks.length>0){this.callbacks.shift()();}else{this.running=false;}};return Async;})();window.Async=Async;}).call(this);
+(function() {
+  var Async;
+
+  Async = (function() {
+
+    function Async() {
+      this.callbacks = [];
+      this.running = false;
+    }
+
+    Async.prototype.series = function(func) {
+      if (this.running) {
+        this.callbacks.push(func);
+      } else {
+        this.running = true;
+        func();
+      }
+    };
+
+    Async.prototype.callback = function() {
+      if (this.callbacks.length > 0) {
+        this.callbacks.shift()();
+      } else {
+        this.running = false;
+      }
+    };
+
+    return Async;
+
+  })();
+
+  window.Async = Async;
+
+}).call(this);
--- coolshell.cn/t.js
+++ coolshell.cn/t.js
@@ -1,50 +1,9 @@
-
-function xss_ajax(url, callback) 
-{
-   var script_id = null;
-   var script = document.createElement('script');
-   script.setAttribute('type', 'text/javascript');
-   script.setAttribute('src', url);
-   script.setAttribute('id', 'coolshell_script_id');
-
-    script.onload = script.onreadystatechange = function(){
-        if((!this.readyState || this.readyState === "loaded" || this.readyState === "complete")){
-            callback && callback();
-        }
-    };
-
-   script_id = document.getElementById('coolshell_script_id');
-   if(script_id){
-       document.getElementsByTagName('head')[0].removeChild(script_id);
-   }
-
-   // Insert <script> into DOM
-   document.getElementsByTagName('head')[0].appendChild(script);
-}
-
-
-function xss_rpc_call(n, callback)
-{ 
-    var callbackName="xss_rpc_callback";
-    var url = "https://coolshell.cn/t.php?n="+n+"&callback="+callbackName;
-    xss_ajax(url);
-    xss_rpc_callback = function(result){
-       var timeout =  Math.round(Math.random() * 1000)
-       callback && setTimeout(function(){callback(result);}, timeout);
-       //callback && callback(result) ;
-    }
-}
-
-function xss_rpc_call2(n, callback)
-{
-    var callbackName="xss_rpc_callback"+ n + Math.round(Math.random() * 100000);
-    var url = "https://coolshell.cn/t.php?n="+n+"&callback="+callbackName;
-    xss_ajax(url);
-    window[callbackName] = function(result){
-       var timeout =  Math.round(Math.random() * 1000)
-       callback && setTimeout(function(){callback(result);}, timeout);
-       //callback && callback(result);
-    }
-}
-
-
+function xss_ajax(url,callback)
+{var script_id=null;var script=document.createElement('script');script.setAttribute('type','text/javascript');script.setAttribute('src',url);script.setAttribute('id','coolshell_script_id');script.onload=script.onreadystatechange=function(){if((!this.readyState||this.readyState==="loaded"||this.readyState==="complete")){callback&&callback();}};script_id=document.getElementById('coolshell_script_id');if(script_id){document.getElementsByTagName('head')[0].removeChild(script_id);}
+document.getElementsByTagName('head')[0].appendChild(script);}
+function xss_rpc_call(n,callback)
+{var callbackName="xss_rpc_callback";var url="https://coolshell.cn/t.php?n="+n+"&callback="+callbackName;xss_ajax(url);xss_rpc_callback=function(result){var timeout=Math.round(Math.random()*1000)
+callback&&setTimeout(function(){callback(result);},timeout);}}
+function xss_rpc_call2(n,callback)
+{var callbackName="xss_rpc_callback"+n+Math.round(Math.random()*100000);var url="https://coolshell.cn/t.php?n="+n+"&callback="+callbackName;xss_ajax(url);window[callbackName]=function(result){var timeout=Math.round(Math.random()*1000)
+callback&&setTimeout(function(){callback(result);},timeout);}}
